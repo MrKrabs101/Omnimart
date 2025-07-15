d6800a6673ee9c8e77a3521f6a1c82d43881705c
ashu <ashu@gmail.com>
2025-07-15 00:37:39 -0400

    committing files

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
+from db import get_db_connection
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return render_template("index.html")
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+    conn.close()
+    return render_template("index.html", products=products)
 
 @app.route("/cart")
-def cartpage():
+def cart():
     return render_template("cart.html")
 
 if __name__ == "__main__":
