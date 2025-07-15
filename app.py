4096ce95370e624a685f104816a011c1e33970bf
ashu <ashu@gmail.com>
2025-07-14 20:51:57 -0400

    integrated the both html

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def homepage():
     return render_template("index.html")
+
+@app.route("/cart")
+def cartpage():
+    return render_template("cart.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
